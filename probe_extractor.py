7925e431b32c0685a345d6f7b5dbea005dd0ceee
anjandash <anjan8@gmail.com>
2022-03-11 20:30:02 +0100

    update

--- probe_extractor.py
+++ probe_extractor.py
@@ -108,7 +108,7 @@ def save_features(model, tokenizer, device):
             for input_ids, input_mask, example_indices in eval_dataloader:
                 input_ids   = input_ids.to(device)    # batch_sized input_ids tensor
                 input_mask  = input_mask.to(device)   # batch_sized input_mask tensor
-                all_outputs = model(input_ids=input_ids, attention_mask=input_mask) # token_type_ids=None,
+                all_outputs = model(input_ids=input_ids)#, attention_mask=input_mask) # token_type_ids=None,
                 enc_layers  = all_outputs.hidden_states
                 #print("***************************************************")
                 #print(model_checkpoint, " => Num layers:", len(enc_layers))
